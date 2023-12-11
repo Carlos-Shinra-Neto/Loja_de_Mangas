d6a46cc4f6f87c2fe5c4308c6fc1edbae0416ef9
Carlos <leosergi09@gmail.com>
2023-12-11 13:55:06 -0300

    Produto criado!

---
+++ src/entities/Produto.java
@@ -0,0 +1,28 @@
+package entities;
+
+abstract class Produto {
+    private String nome;
+    private int id;
+    private double preco;
+
+    public Produto(String nome, double preco) {
+        this.nome = nome;
+        this.id = LojaMangas.gerarProximoId();
+        this.preco = preco;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public abstract void exibirInfo();
+}
